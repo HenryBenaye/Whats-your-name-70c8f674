a7fb4681984526bb4281b7d0449d2a49b354f86a
HENRY BENAYE <2106747@talnet.nl>
2020-01-14 13:46:46 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,25 @@
+<?php 
+// $a 
+// $b 
+// echo "Wat is je voornaam";
+// $a = readline("");
+
+// echo "Wat is je achternaam";
+// $b = readline("");
+
+// echo $a, $b;
+
+$a = "Wat is je voornaam?";
+$b = "Wat is je achternaam?";
+
+echo $a . PHP_EOL;
+$voorNaam = readline("");
+echo $b . PHP_EOL;
+$achterNaam = readline("");
+echo ("Jou naam is"). $voorNaam. $achterNaam;
+
+
+
+
+
+
